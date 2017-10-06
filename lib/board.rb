5373090bf0b8889fe01d7d2b38bd889d4d481333
Harvey Hodder <17135365@midkent.ac.uk>
2017-10-06 16:09:47 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,2 @@
 # Define the variable board below.
-board = [" ", " ", " "]
-
-# Position 1: Left
-board[0]
-
-# Position 2: Middle
-board[1]
-
-# Position 3: Right
-board[2]
-
-# Move X to the Position 2, Middle
-board[1] = "X"
-board #=> [" ", "X", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
